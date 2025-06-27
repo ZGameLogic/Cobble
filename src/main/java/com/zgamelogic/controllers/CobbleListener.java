c4edba374bf203adcda39001d21187d90d47cfcc
Ben Shabowski <benshabowski@gmail.com>
2025-06-26 20:01:58 -0500

    Adding in functionality

--- src/main/java/com/zgamelogic/controllers/CobbleListener.java
+++ src/main/java/com/zgamelogic/controllers/CobbleListener.java
@@ -1,16 +1,180 @@
 package com.zgamelogic.controllers;
 
+import com.zgamelogic.data.CobbleServiceException;
+import com.zgamelogic.data.building.CobbleBuilding;
+import com.zgamelogic.data.building.CobbleBuildingRepository;
+import com.zgamelogic.data.enums.CobbleBuildingType;
+import com.zgamelogic.data.npc.CobbleNpc;
+import com.zgamelogic.data.player.CobblePlayer;
 import com.zgamelogic.discord.annotations.DiscordController;
+import com.zgamelogic.discord.annotations.DiscordMapping;
+import com.zgamelogic.discord.annotations.EventProperty;
+import com.zgamelogic.services.CobbleHelperService;
+import com.zgamelogic.services.CobbleService;
+import com.zgamelogic.services.ResourceService;
+import lombok.AllArgsConstructor;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.utils.FileUpload;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @DiscordController
+@AllArgsConstructor
 public class CobbleListener {
+    private final ResourceService resourceService;
+    private final CobbleHelperService helperService;
+    private final CobbleService cobbleService;
+    private final CobbleBuildingRepository cobbleBuildingRepository;
+
+    @DiscordMapping(Id = "cobble", SubId = "help")
+    private void cobbleHelp(SlashCommandInteractionEvent event) throws IOException {
+        event
+            .replyFiles(FileUpload.fromData(resourceService.getCobbleLogo().getInputStream(), "cobble-logo.png"))
+            .addEmbeds(helperService.getHelpMessage(1))
+            .addActionRow(Button.secondary("cobble-help-page-prev", "Previous page").asDisabled(), Button.secondary("cobble-help-page-next", "Next Page"))
+            .queue();
+    }
+
+    @DiscordMapping(Id = "cobble", SubId = "start")
+    private void cobbleStart(SlashCommandInteractionEvent event, @EventProperty(name = "town-name") String townName) {
+        try {
+            if(townName == null || townName.isEmpty()) townName = event.getUser().getName() + "'s town";
+            CobblePlayer player = cobbleService.startCobblePlayer(event.getUser().getIdLong(), townName);
+            event
+                .replyFiles(FileUpload.fromData(resourceService.mapAppearanceAsStream(player.getNpcs().get(0).getAppearance()), "npc.png"))
+                .addEmbeds(helperService.getStartMessage(player))
+                .queue();
+        } catch (CobbleServiceException | IOException e) {
+            event.reply(e.getMessage()).setEphemeral(true).queue();
+        }
+    }
+
+    @DiscordMapping(Id = "cobble", SubId = "citizens")
+    private void cobbleCitizens(
+        SlashCommandInteractionEvent event,
+        @EventProperty String citizen
+    ) {
+        boolean citizenIncluded = citizen != null && !citizen.isEmpty();
+        try {
+            if (citizenIncluded) {
+                helperService.cobbleCitizen(event, citizen);
+            } else {
+                helperService.cobbleCitizens(event);
+            }
+        } catch (CobbleServiceException | IOException e) {
+            event.reply(e.getMessage()).setEphemeral(true).queue();
+        }
+    }
+
+    @DiscordMapping(Id = "cobble", SubId = "citizens", FocusedOption = "citizen")
+    private void cobbleCitizensAutocomplete(
+        CommandAutoCompleteInteractionEvent event,
+        @EventProperty String citizen
+    ){
+        try {
+            List<CobbleNpc> npcs = cobbleService.getCobbleNpcs(event.getUser().getIdLong());
+            event.replyChoices(npcs.stream()
+                .filter(npc -> citizen.isEmpty() || npc.getFirstName().toLowerCase().contains(citizen.toLowerCase()) ||  npc.getLastName().toLowerCase().contains(citizen.toLowerCase()))
+                .map(npc -> new Command.Choice(npc.getFirstName() + " " + npc.getLastName(), npc.getId().toString()))
+                .toList()
+            ).queue();
+        } catch (CobbleServiceException e) {
+            event.replyChoices(List.of()).queue();
+        }
+    }
+
+    @DiscordMapping(Id = "cobble", GroupName = "building", SubId = "codex", FocusedOption = "building")
+    private void cobbleBuildingCodexAutocomplete(
+        CommandAutoCompleteInteractionEvent event,
+        @EventProperty String building
+    ){
+        event.replyChoices(Arrays.stream(CobbleBuildingType.values())
+            .filter(type -> building.isEmpty() || type.getFriendlyName().toLowerCase().replaceAll("'", "").contains(building.toLowerCase()))
+            .map(type -> new Command.Choice(type.getFriendlyName(), type.getFriendlyName()))
+            .toList()
+        ).queue();
+    }
+
+    @DiscordMapping(Id = "cobble", GroupName = "building", SubId = "codex")
+    private void cobbleBuildingCodex(
+        SlashCommandInteractionEvent event,
+        @EventProperty String building
+    ) throws CobbleServiceException {
+        int page = 1;
+        int maxPage = cobbleService.getCobbleBuildingList().size();
+        if(building != null && CobbleBuildingType.validName(building)) {
+            page = CobbleBuildingType.fromName(building).ordinal() + 1;
+        }
+        event.replyEmbeds(helperService.getBuildingMessage(page))
+            .addActionRow(
+                Button.secondary("cobble-building-codex-page-prev", "Previous page").withDisabled(page == 1),
+                Button.secondary("cobble-building-codex-page-next", "Next Page").withDisabled(page == maxPage)
+            )
+            .queue();
+    }
+
+    @DiscordMapping(Id = "cobble", GroupName = "rename", SubId = "building", FocusedOption = "name")
+    private void cobbleRenameBuildingAutocomplete(
+        CommandAutoCompleteInteractionEvent event,
+        @EventProperty String building
+    ){
+        event.replyChoices(cobbleBuildingRepository.findAllByPlayer_PlayerId(event.getUser().getIdLong()).stream()
+            .filter(cb -> building == null || building.isEmpty() || cb.getBuildingName().toLowerCase().contains(building.toLowerCase()))
+            .map(cb -> new Command.Choice(cb.getBuildingName(), cb.getCobbleBuildingId().toString()))
+            .toList()
+        ).queue();
+    }
+
+    @DiscordMapping(Id = "cobble", GroupName = "rename", SubId = "building")
+    private void cobbleRenameBuildingSlashCommand(
+        SlashCommandInteractionEvent event,
+        @EventProperty String name,
+        @EventProperty(name = "new-name") String newName
+    ){
+        try {
+            CobbleBuilding building = cobbleService.getCobbleBuilding(event.getUser().getIdLong(), name);
+            cobbleService.renameBuilding(building, newName);
+            event.reply("Building successfully renamed to " + newName).setEphemeral(true).queue();
+        } catch (CobbleServiceException e) {
+            event.reply(e.getMessage()).setEphemeral(true).queue();
+        }
+    }
+
+    @DiscordMapping(Id = "cobble", GroupName = "rename", SubId = "town")
+    private void cobbleRenameTownSlashCommand(
+        SlashCommandInteractionEvent event,
+        @EventProperty(name = "new-name") String name
+    ){
+        try {
+            CobblePlayer player = cobbleService.getCobblePlayer(event.getUser().getIdLong());
+            cobbleService.renameTown(player, name);
+            event.reply("Town successfully renamed to " + name).setEphemeral(true).queue();
+        } catch (CobbleServiceException e) {
+            event.reply(e.getMessage()).setEphemeral(true).queue();
+        }
+    }
+
+    @DiscordMapping(Id = "cobble-help-page-next")
+    private void cobbleHelpPageNext(ButtonInteractionEvent event){ helperService.cobbleHelpPage(event); }
+    @DiscordMapping(Id = "cobble-help-page-prev")
+    private void cobbleHelpPagePrev(ButtonInteractionEvent event){ helperService.cobbleHelpPage(event); }
+
+    @DiscordMapping(Id = "cobble-building-codex-page-next")
+    private void cobbleBuildingCodexPageNext(ButtonInteractionEvent event){ helperService.cobbleBuildingCodexPage(event); }
+    @DiscordMapping(Id = "cobble-building-codex-page-prev")
+    private void cobbleBuildingCodexPagePrev(ButtonInteractionEvent event){ helperService.cobbleBuildingCodexPage(event); }
+
     @Bean
     public List<CommandData> cobbleCommands(){
         return List.of(
